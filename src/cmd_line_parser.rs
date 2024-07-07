eeb1a5db36ce3652f7bc0a2e9ddd320f8333f000
Anna <45910905+CatAnnaDev@users.noreply.github.com>
2024-07-07 12:08:06 +0200

    Add files via upload

---
+++ src/cmd_line_parser.rs
@@ -0,0 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author = "PsykoDev", version, about, long_about = None)]
+pub struct Args {
+    #[arg(short = 'd', long, help = "domain name \"exemple.com\"")]
+    pub domain: String,
+
+    #[arg(short = 'f', long, help = "wordlist \"wordlist/subdomain.txt\"")]
+    pub word_list_path: String,
+
+    #[arg(
+        short = 't',
+        long,
+        default_value_t = 1,
+        help = "Thread use to scan max 5"
+    )]
+    pub thread: usize,
+}
